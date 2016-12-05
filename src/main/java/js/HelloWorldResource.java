844889e6e734333e86003ea83eace20931f8fb5c
Nicholas Aguirre <naa785@cs.utexas.edu>
2016-12-04 23:12:11 -0600

    trying

--- src/main/java/js/HelloWorldResource.java
+++ src/main/java/js/HelloWorldResource.java
@@ -1,21 +1,95 @@
 package js;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.ListIterator;
+import java.util.Set;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import org.jsoup.*;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 @Path("/path")
 public class HelloWorldResource {
 	
+
+	
 	public HelloWorldResource() {
-		
+
 	}
 	
 	@GET
-	@Path("/helloworld-resource")
+	@Path("/solum")
 	@Produces("text/html")
-	public String helloWorld() {
-		System.out.println("Inside helloworld");
-		return "Hello world ";
+	public String getAllProjects() throws Exception {
+		ArrayList<String> parsed = getYears();
+//		Set<String> keys = parsed.keySet();
+		
+		HashMap<String, Integer> meetings = getMeetings(parsed);
+		Set<String> keys = meetings.keySet();
+		String str = "";
+		for(String item : keys){
+			str += item + "~" + meetings.get(item) + "~";
+		}
+		
+		return str;
+	}
+
+	public ArrayList<String> getYears() throws IOException{
+		ArrayList<String> res = new ArrayList<String>();
+		Document doc = Jsoup.connect("http://eavesdrop.openstack.org/meetings/solum_team_meeting/").get();
+		Elements links = doc.select("body a");
+	    if (links != null) {
+		    ListIterator<Element> iter = links.listIterator();		    	
+		    while(iter.hasNext()) {
+	    		Element e = (Element) iter.next();
+	    		String s = e.html();
+	    		if ( s != null && s.matches("^\\d{4}/")) {
+	    			res.add(s);
+	    		}
+		    }
+	    }
+		return res;
+	}
+	
+	public HashMap<String, Integer> getMeetings(ArrayList <String> years) throws IOException{
+		HashMap<String, Integer> res = new HashMap<String, Integer>();
+		for(String item : years){
+			HashMap<String, Integer> num = new HashMap<String, Integer>();
+			Integer count = 0;
+			Document doc = Jsoup.connect("http://eavesdrop.openstack.org/meetings/solum_team_meeting/" + item).get();
+			Elements links = doc.select("body a");
+		    if (links != null) {
+			    ListIterator<Element> iter = links.listIterator();		    	
+			    while(iter.hasNext()) {
+		    		Element e = (Element) iter.next();
+		    		String s = e.html();
+		    		if ( s != null && s.contains("solum_team_meeting")) {
+//		    			System.out.println(s);
+		    			String split[] = s.split("\\.");
+		    			String secsplit[] = split[1].split("-");
+		    			String r = split[0] + secsplit[0] + secsplit[1] + secsplit[2];
+		    			num.put(r, res.get(s));
+		    		}
+			    }
+		    }
+		    if(item.equals("2014/")){
+		    	Set<String> keys = num.keySet();
+		    	for(String yo : keys){
+//		    		System.out.println(yo);
+		    	}
+		    }
+		    res.put(item, num.size());
+		}
+		return res;
 	}
 }
--- src/main/webapp/myscript.js
+++ src/main/webapp/myscript.js
@@ -3,21 +3,27 @@
  */
 
 function myFunction() {
-    var x, text;
-
-    // Get the value of input field with id="numb"
-
-    x = document.getElementById("numb").value;
-
-    // If x is Not a Number or less than one or greater than 10
-
-    if (isNaN(x) || x < 1 || x > 10) {
-        text = "Invalid input.";
-    } else {
-        text = "Correct input.";
-    }
-    document.getElementById("demo").innerHTML = text;
-    
-    console.log("Testing console.");
-    console.log(text);
+	var content;
+	var url = new XMLHttpRequest();
+	url.open("GET", "/js-example/app/path/solum", false);
+	url.send();
+	content = url.response;
+	document.getElementById("tablespace").border = 1;
+	var table = "<table width='50%' style='border-style : solid; border-width : 3px;'>" +
+			"<tr>" +
+			"<td bgcolor='#DDDDDD' style='border-style : solid; border-width : 3px;'>Years</td>" +
+			"<td bgcolor='#DDDDDD' style='border-style : solid; border-width : 3px;'>Meetings</td>" +
+			"</tr>";
+	content = content.split("~");
+	for (var i = 0; i < content.length - 1; i+=2){
+		var year = content[i].substring(0, 4);
+		var num = content[i+1];
+		table += "<tr>" +
+		"<td style='border-style : solid; border-width : 2px;'>" + year + "</td>" +
+		"<td style='border-style : solid; border-width : 2px;'>" + num + "</td>" +
+		"</tr>";
+	}
+	table += "</table>";
+	document.getElementById("tablespace").innerHTML = table;
+	
 }
